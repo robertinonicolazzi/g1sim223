61757dc7c06f924feb52b83aa8a15a771660f705
Robertino Nicolazzi <robertino.nicolazzi@gmail.com>
2026-04-15 10:48:54 -0300

    config fastdds

--- g1_sim_controller/ros_entrypoint.sh
+++ g1_sim_controller/ros_entrypoint.sh
@@ -25,4 +25,6 @@ case "$SLAM_MODE" in
     ;;
 esac
 
+export FASTRTPS_DEFAULT_PROFILES_FILE=/ros_ws/install/g1_sim_controller/share/g1_sim_controller/config/fastdds_profile.xml
+
 exec ros2 launch g1_sim_controller bringup.launch.py $LAUNCH_ARGS
